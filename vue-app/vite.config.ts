6077da53eca224652118578f9667d79ddd758e03
Victor Seara <victor.seara@dtidigital.com.br>
2023-07-28 17:52:14 -0300

    feat: setup vite library mode to output the widget properly

--- vue-app/vite.config.ts
+++ vue-app/vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue({
+    customElement: true
+  })],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/widget.ts'),
+      name: "Widget",
+      fileName: "widget/index", 
+    },   
+  }, 
+  define: {
+    'process.env.NODE_ENV': '"production"'
+  }, 
 })
+ 
