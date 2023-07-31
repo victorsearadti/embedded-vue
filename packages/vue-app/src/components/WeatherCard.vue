e5c55b8d78e0b432295271b0c0aba8844c5763d7
victorseara <victorseara.me@gmail.com>
2023-07-31 01:14:17 -0300

    refactor: remove padding

--- packages/vue-app/src/components/WeatherCard.vue
+++ packages/vue-app/src/components/WeatherCard.vue
@@ -14,7 +14,7 @@ export type WeatherCardProps = {
 defineProps<WeatherCardProps>();
 </script>
 <template>
-  <v-card class="pa-8 bg-vite-black" :elevation="0">
+  <v-card class="bg-vite-black" :elevation="0">
     <div>
       <v-card-item :title="title" :prepend-avatar="avatar">
         <template v-slot:subtitle>
